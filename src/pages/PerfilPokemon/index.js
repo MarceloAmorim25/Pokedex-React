f31bbc6351000188d3b37201635990dfb3f26c9f
Marcelo Medeiros Amorim <58489782+MarceloAmorim25@users.noreply.github.com>
2020-05-30 21:42:30 -0300

    Update index.js

--- src/pages/PerfilPokemon/index.js
+++ src/pages/PerfilPokemon/index.js
@@ -1,47 +1,65 @@
 import React from 'react';
 import Navbar from '../../navbar';
 import axios from 'axios';
+import PokeBall from '../../assets/PokeBall.png';
+import './estilo.css';
 
 export default function PerfilPokemon() {
 
-    //GET /pokemons/:name
-    //Substitua ​:name​ pelo identificador de um pokémon 
-    //específico para receber as informaçõesdo pokémon específico
-
-
     const [data, setData] = React.useState([]);
-    const [pokemon, setPokemon] = React.useState('');
-   
 
-    let pokeRec = localStorage.getItem('pokemon');
-    setPokemon(pokeRec);
+    //recupera no localStorage o nome do pokemon clicado
 
-   
-   axios
-      .get(`https://pokedex20201.herokuapp.com/pokemons/${pokemon}`)
-      .then((res) =>
-            setData(res.data)       
-      )
-      .catch((error) => 
-            console.log(error)
-      )
-   
-   console.log(data);
+    let descriptionPokemon = localStorage.getItem('pokemons');
+
+
+   //GET => dados do pokemon registrado no localStorage
+
+    React.useEffect(() => {
+       axios
+          .get(`https://pokedex20201.herokuapp.com/pokemons/${descriptionPokemon}`)
+          .then((res) => setData(res.data));
+    }, [descriptionPokemon]);
+
+    console.log(data);
+
+   //Essa é responsável pela captura do Pokemon
+   //Primeiro o preventDefault para evitar que a página recarregue
+   //Depois o post para API com o nome do usuário logado e o pokemon da página de descrição
+ 
+  function handleFav(e) {
+
+     e.preventDefault();
+
+     let username = localStorage.getItem('usuario');
+      
+         axios
+           .post(`https://pokedex20201.herokuapp.com/users/${username}/starred/${descriptionPokemon}`)
+           .then(() => {
+             alert('Pokemon capturado!');
+           })
+           .catch(function (error) {
+             console.log(error);
+             alert('Não foi possível capturar esse Pokemon!');
+           });
+  }
 
    return(
       <>
-         <Navbar />
+         <Navbar />  
 
          <div>
-            <img src={data.image_url} alt="pokemon"></img>
-               <div>{data.kind}</div>
-               <div>{data.name}</div>
-               <div>{data.weight}</div>
-               <div>{data.height}</div>
+            <div><h1>{data.name}</h1></div>
+            <div><h1><img src={data.image_url} alt="pokemon"/></h1></div>
+            <div><h1>{data.height}</h1></div>
+            <div><h1>{data.weight}</h1></div>
+            <div><h1>{data.kind}</h1></div>
+            <div><button onClick={handleFav}><img src={PokeBall} alt="pokemon"/></button></div>            
          </div>
          
       </>
        
     );
 }
+
 
