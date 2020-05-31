1c1115c6848905efaf2f1f632f619593b9a46073
Marcelo Medeiros Amorim <58489782+MarceloAmorim25@users.noreply.github.com>
2020-05-30 22:21:46 -0300

    Update index.js

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -7,33 +7,61 @@ export default function Login() {
 
     
     const[usuario, setUsuario] = useState([]);
+    const[data, setData] = useState([]);
     const history = useHistory();
-    const usuarioAtual = localStorage.getItem('usuario');
+    let list = [];
+    let login = 0;
+    // const usuarioAtual = localStorage.getItem('usuario');
 
+    React.useEffect(() => {
+      axios
+         .get(`https://pokedex20201.herokuapp.com/users`)
+         .then((res) => {
+           setData(res.data.data)
+           console.log(res.data.data)
+          });
+   }, []);
+
+   data.map(k => {
+     list.push(k.username);
+     return list;
+   })
+
+   for(let i=0; i<list.length; i++) {
+     if(list[i] === usuario) {
+        login = 1;
+     }
+
+   } 
+
+   console.log(list);
 
    async function handleLogin(e) {
         e.preventDefault();
 
         localStorage.setItem('usuario', usuario);
-        
-        if(usuario !== usuarioAtual) {
+             
+        if(login === 1) {
+
+          history.push('/home');
+          alert('Login autorizado...');
+
+        }else {
             await axios
               .post('https://pokedex20201.herokuapp.com/users', {
                 username: usuario
               })
               .then((res) => {
                 console.log(res);
+                alert('Novo usuário criado!');
                 history.push('/home');                
               })
               .catch(function (error) {
                 console.log(error);
                 alert('Falha no login, tente novamente.');
               });
-        }else {
-            history.push('/home');
         }
     }
-
 
     return(
 
